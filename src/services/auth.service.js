8cf47cfe83875140d253fe6bc5f251a92583cf35
hamoudilaz <hamoudi_lazkani@hotmail.se>
2025-11-01 01:32:54 +0100

    Implmenet auth

---
+++ src/services/auth.service.js
@@ -0,0 +1,86 @@
+import logger from '#config/logger.js';
+import bcrypt from 'bcrypt';
+import { eq } from 'drizzle-orm';
+import { db } from '#config/database.js';
+import { users } from '#models/user.model.js';
+
+export const hashPassword = async pass => {
+  try {
+    return await bcrypt.hash(pass, 10);
+  } catch (e) {
+    logger.error('Error hashing password:', e);
+    throw new Error('Error hashing');
+  }
+};
+
+export const comparePassword = async (password, hash) => {
+  try {
+    return await bcrypt.compare(password, hash);
+  } catch (e) {
+    logger.error('Error comparing password:', e);
+    throw new Error('Error comparing password');
+  }
+};
+
+export const authenticateUser = async ({ email, password }) => {
+  try {
+    const existingUsers = await db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1);
+
+    if (existingUsers.length === 0) {
+      throw new Error('User with this email does not exist');
+    }
+
+    const user = existingUsers[0];
+    const isPasswordValid = await comparePassword(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new Error('Invalid password');
+    }
+
+    logger.info(`User ${user.email} authenticated successfully`);
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      created_at: user.created_at,
+    };
+  } catch (e) {
+    logger.error(`Error authenticating user: ${e.message}`);
+    throw e;
+  }
+};
+
+export const createUser = async ({ name, email, password, role = 'user' }) => {
+  try {
+    const existingUsers = await db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1);
+
+    if (existingUsers.length > 0) throw new Error('User with this email already exists');
+
+    const password_hash = await hashPassword(password);
+
+    const [newUser] = await db
+      .insert(users)
+      .values({ name, email, password: password_hash, role })
+      .returning({
+        id: users.id,
+        name: users.name,
+        email: users.email,
+        role: users.role,
+        created_at: users.created_at,
+      });
+    logger.info(`User ${newUser.email} created successfully`);
+    return newUser;
+  } catch (e) {
+    logger.error(`Error creating the user: ${e}`);
+    throw e;
+  }
+};
